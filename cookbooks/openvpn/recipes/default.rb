183a2a425adb86e6743ef5cac683586b299eb9c5
James Golick <james@giraffesoft.ca>
2009-05-24 13:27:05 -0400

    setup packages and bridge interface

--- cookbooks/openvpn/recipes/default.rb
+++ cookbooks/openvpn/recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "openvpn" do
+  action :install
+end
+
+package "bridge-utils" do
+  action :install
+end
+
+execute "Add br interface" do
+  line = "auto lo br0"
+  command "echo '#{line}' >> /etc/network/interfaces && /etc/init.d/networking restart"
+  not_if  "cat /etc/network/interfaces | grep -q #{line}"
+end
+
